8b4137191e323d3e1cb5a4d060badeba3d65cc12
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2025-07-06 12:44:19 +0530

    Add files via upload

---
+++ plugins/channel.js
@@ -0,0 +1,12 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪ *𝐅𝖾ᥣ𝗂𝗑* ✪★⭑⭑⭑
+│Song Download Karne Ke liye Gc Join Kare 🌟
+│ 📝 https://chat.whatsapp.com/LOcqAvx03ElCpEoFy2zxdP?mode=r_c
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['play']
+handler.tags = ['main']
+handler.command = ['play' ] 
+
+export default handler
