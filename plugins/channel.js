a8cbabb0967d4c18cb70b00acea445ce63aefdc1
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2025-08-18 21:38:54 +0530

    Update channel.js

--- plugins/channel.js
+++ plugins/channel.js
@@ -2,7 +2,7 @@ let handler = async m => m.reply(`
 
 ╭⭑⭑⭑★✪ *𝐅𝖾ᥣ𝗂𝗑* ✪★⭑⭑⭑
 │Song Download Karne Ke liye Gc Join Kare 🌟
-│ 📝 https://chat.whatsapp.com/LOcqAvx03ElCpEoFy2zxdP?mode=r_c
+│ ✨ https://chat.whatsapp.com/CXNhr025frx6EgSuof4Abx?mode=ems_copy_c
 ╰━━━━━━━━━━━━━━━━╯
 `.trim())
 handler.help = ['play']
