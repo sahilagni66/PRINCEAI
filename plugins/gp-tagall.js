cd491f2fa18ba094b990bb83ace0e9c37a9be06d
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2025-08-30 20:24:18 +0530

    Update gp-tagall.js

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -1,26 +1,15 @@
 let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetadata }) => {
-    const botNumber = conn.user.jid;
-    const users = participants.map(u => u.id).filter(v => v !== conn.user.jid);
-    if (m.sender !== botNumber && !isOwner && !isAdmin) {
-        m.reply("🛡️ This command is only for *Group Admins*");
-        return;
-    }
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    m.reply(`͏> 𝐍 𝐎 𝐓 𝐈 𝐅 𝐈 𝐂 𝐀 𝐓 𝐈 𝐎 𝐍 
+*𝗅 ͜͝    𝗅 ͜͝    𝗅 ͜͝     ׄ𖦹ి֢      ͜͝ 𝗅     ͜͝ 𝗅    ͜͝ 𝗅*\n\n𝐌𝖾𝗆𝖻𝖾𝗋 : *${participants.length}*${text ? `\n🌟 𝐌𝖾𝗌𝗌𝖺𝗀𝖾 : ${text}\n` : ''}\n` + users.map(v => '*𓈒  ▬᳢̅ ⃞ 🌟* @' + v.replace(/@.+/, '')).join`\n` + '\n *ׅ𒁴 ׄ֪﹝𝚻ɧᥱᥱꪀᲫ﹞֪ׄ* ', null, {
+        mentions: users
+    })
+}
 
-    const groupInfo = `┃💗⊹ 𝗚𝗥𝗢𝗨𝗣 : *${groupMetadata.subject}*\n\n`;
-    const membersInfo = `┃💗⊹ 𝗠𝗘𝗠𝗕𝗘𝗥𝗦 : *${participants.length}*${text ? `\n┃💗⊹ 𝗠𝗘𝗦𝗦𝗔𝗚𝗘 : ${text}\n` : ''}`;
-    const mentions = users.map(v => '┃💗⊹ @' + v.replace(/@.+/, '')).join`\n`;
-    const footer = '\n└──✪ ⚡𝑷-𝑴𝑫⚡ ┃ ᴮᴼᵀ ✪──';
+handler.help = ['tagall']
+handler.tags = ['group']
+handler.command = ['tagall']
+handler.admin = false
+handler.group = true
 
-    const tagAllMessage = `${groupInfo}${membersInfo}\n\n┌───⊷ 𝗠𝗘𝗡𝗧𝗜𝗢𝗡𝗦\n${mentions}${footer}`;
-
-
-    m.reply(tagAllMessage, null, { mentions: users });
-};
-
-handler.help = ['tagall'];
-handler.tags = ['group'];
-handler.command = ['tagall', 'invo'];
-handler.admin = false; 
-handler.group = true; 
-
-export default handler;
+export default handler
