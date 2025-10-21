debe165298f24509b393d72e39d78f8eaad92382
sahilagni66 <145852964+sahilagni66@users.noreply.github.com>
2025-10-21 22:08:02 +0530

    Update message formatting and change admin flag

--- plugins/gp-tagall.js
+++ plugins/gp-tagall.js
@@ -10,16 +10,16 @@ let handler = async (m, { conn, text, participants, isAdmin, isOwner, groupMetad
 
   // Build message
   const message = 
-`▢ Group : *${groupMetadata.subject}*
-▢ Total Members : *${participants.length}*
-${text ? `▢ Message : ${text}\n` : ''}
-┌───⊷ *ADMINS*
-${admins.map(v => '▢ @' + v.replace(/@.+/, '')).join('\n') || '▢ None'}
-└───────────────
+`𝐆𝗋ⱺυρ: *${groupMetadata.subject}*
+𝐓α𝗍αᥣ 𝐌𝖾ꭑᑲ𝖾𝗋𝗌: *${participants.length}*
+${text ? `𝐌𝖾𝗌𝗌𝖺𝗀𝖾: ${text}\n` : ''}
+︵꯭፝֟⏜۫᷼︵ׅ𝐀ᑯꭑ𝗂𐓣𝗌︵꯭፝֟⏜۫᷼︵ׅ
+${admins.map(v => '𓈒  ▬᳢̅ ⃞ 🌟 @' + v.replace(/@.+/, '')).join('\n') || '▢ None'}
+𝐌⃝𝗂𐓣ᑯ 𝗒ⱺυ'𝗋𝖾 ⱺω𐓣 ᑲυ𝗌𝗂𐓣𝖾𝗌𝗌𓋜
 
-┌───⊷ *MEMBERS*
-${members.map(v => '▢ @' + v.replace(/@.+/, '')).join('\n') || '▢ None'}
-└──✪ GURU ┃ ᴮᴼᵀ ✪──`
+͏██  ͏██ ͏.𝐌𝖾𝗆𝖻𝖾𝗋. ͏██  ͏██
+${members.map(v => ' ━ ✦ ⃞🥮ᩧᩙᩪᩩ̶̷  ͟ ͟ ͟ ͟  @' + v.replace(/@.+/, '')).join('\n') || '▢ None'}
+𝐌⃝𝗂𐓣ᑯ 𝗒ⱺυ'𝗋𝖾 ⱺω𐓣 ᑲυ𝗌𝗂𐓣𝖾𝗌𝗌𓋜`
 
   // Reply and mention all
   m.reply(message, null, {
@@ -31,7 +31,7 @@ handler.help = ['tagall']
 handler.tags = ['group']
 handler.command = ['tagall']
 handler.desc = 'Tag all group members separately (Admins & Members)'
-handler.admin = true
+handler.admin = false
 handler.group = true
 
 export default handler
